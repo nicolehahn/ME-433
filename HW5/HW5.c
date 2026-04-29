02ca143ca1d688706ccfc3d015e1387661227cf3
nicoleahahn <139174818+nicoleahahn@users.noreply.github.com>
2026-04-28 21:56:47 -0500

    Got sensors working. Added display files

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "HW5.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+
 
 
 
@@ -20,12 +19,112 @@ int main()
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    write_to_chip(PWR_MGMT_1, 0x00); // turn on chip
+    write_to_chip(ACCEL_CONFIG, 0x00); // set accelerometer sensitivity to +- 2g
+    write_to_chip(GYRO_CONFIG, 0x18); // set gyro sensitvity to +- 2000 degrees
 
     while (true) {
-        printf("Hello, world!\n");
         sleep_ms(1000);
+
+        // read all 14 output registers
+        uint8_t data[14];
+
+        i2c_write_blocking(i2c_default, ADDR, (uint8_t[]){ACCEL_XOUT_H}, 1, true);
+        i2c_read_blocking(i2c_default, ADDR, data, 14, false);
+
+        // combine data
+        int16_t a_xout = combine(data[0], data[1]);
+        int16_t a_yout = combine(data[2], data[3]);
+        int16_t a_zout = combine(data[4], data[5]);
+
+        int16_t t_out  = combine(data[6], data[7]);
+
+        int16_t g_xout = combine(data[8], data[9]);
+        int16_t g_yout = combine(data[10], data[11]);
+        int16_t g_zout = combine(data[12], data[13]);
+
+        /* accelerometer
+        uint8_t a_xout_h = read_chip(ACCEL_XOUT_H);
+        uint8_t a_xout_l = read_chip(ACCEL_XOUT_L);
+        uint8_t a_yout_h = read_chip(ACCEL_YOUT_H);
+        uint8_t a_yout_l = read_chip(ACCEL_YOUT_L);
+        uint8_t a_zout_h = read_chip(ACCEL_ZOUT_H);
+        uint8_t a_zout_l = read_chip(ACCEL_ZOUT_L);
+
+        // gyroscope
+        uint8_t g_xout_h = read_chip(GYRO_XOUT_H);
+        uint8_t g_xout_l = read_chip(GYRO_XOUT_L);
+        uint8_t g_yout_h = read_chip(GYRO_YOUT_H);
+        uint8_t g_yout_l = read_chip(GYRO_YOUT_L);
+        uint8_t g_zout_h = read_chip(GYRO_ZOUT_H);
+        uint8_t g_zout_l = read_chip(GYRO_ZOUT_L);
+
+        // thermometer
+        uint8_t t_out_h = read_chip(TEMP_OUT_H);
+        uint8_t t_out_l = read_chip(TEMP_OUT_L);
+
+        // combine into 16 bit signed ints
+        int16_t a_xout = combine(a_xout_h, a_xout_l);
+        int16_t a_yout = combine(a_yout_h, a_yout_l);
+        int16_t a_zout = combine(a_zout_h, a_zout_l);
+
+        int16_t g_xout = combine(g_xout_h, g_xout_l);
+        int16_t g_yout = combine(g_yout_h, g_yout_l);
+        int16_t g_zout = combine(g_zout_h, g_zout_l);
+
+        int16_t t_out = combine(t_out_h, t_out_l);
+
+        */
+
+        // turn outputs into readable values;
+
+        float a_x = a_xout*0.000061;
+        float a_y = a_yout*0.000061;
+        float a_z = a_zout*0.000061;
+
+        float g_x = g_xout*0.007630;
+        float g_y = g_yout*0.007630;
+        float g_z = g_zout*0.007630;
+
+        float t = (t_out/340.0) + 36.53;
+
+        // print outputs
+        printf("x accel: %f\r\n", a_x);
+        printf("y accel: %f\r\n", a_y);
+        printf("z accel: %f\r\n", a_z);
+
+        printf("x gyro: %f\r\n", g_x);
+        printf("y gyro: %f\r\n", g_y);
+        printf("z gyro: %f\r\n", g_z);
+
+        printf("temp: %f\r\n", t);
+
+
     }
+
+
 }
+
+
+void write_to_chip(uint8_t reg, uint8_t data){
+    uint8_t buf[2];
+    buf[0] = reg;
+    buf[1] = data;
+    i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+}
+
+uint8_t read_chip(uint8_t reg){
+    uint8_t buf;
+
+    i2c_write_blocking(i2c_default, ADDR, &reg, 1, true);  // true to keep host control of bus
+    i2c_read_blocking(i2c_default, ADDR, &buf, 1, false);  // false - finished with bus
+
+    return buf;
+}
+
+int16_t combine(uint8_t high, uint8_t low){
+    int16_t val = (int16_t)((high << 8) | low);
+    return val;
+}
+
---
+++ HW5/HW5.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#define I2C_PORT i2c0
+#define I2C_SDA 12
+#define I2C_SCL 13
+#define ADDR 0x68
+
+// config registers
+#define CONFIG 0x1A
+#define GYRO_CONFIG 0x1B
+#define ACCEL_CONFIG 0x1C
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_2 0x6C
+// sensor data registers:
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_XOUT_L 0x3C
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_YOUT_L 0x3E
+#define ACCEL_ZOUT_H 0x3F
+#define ACCEL_ZOUT_L 0x40
+#define TEMP_OUT_H   0x41
+#define TEMP_OUT_L   0x42
+#define GYRO_XOUT_H  0x43
+#define GYRO_XOUT_L  0x44
+#define GYRO_YOUT_H  0x45
+#define GYRO_YOUT_L  0x46
+#define GYRO_ZOUT_H  0x47
+#define GYRO_ZOUT_L  0x48
+#define WHO_AM_I     0x75
+
+// function declarations
+void write_to_chip(uint8_t reg, uint8_t data);
+uint8_t read_chip(uint8_t reg);
+int16_t combine(uint8_t high, uint8_t low);
