7a71890d2e6e6651fdbb1bc1b61861f16b341cea
nicoleahahn <139174818+nicoleahahn@users.noreply.github.com>
2026-05-28 08:49:47 -0500

    Finish MAF Filter code

--- HW9/Filtering.py
+++ HW9/Filtering.py
@@ -2,47 +2,82 @@ import csv
 import matplotlib.pyplot as plt # for plotting
 import numpy as np # for sine function
 
-t = [] # column 0
-data1 = [] # column 1
+def fft2(t1, y1, t2, y2):
+    Fs = 10000 # sample rate
 
-with open('sigA.csv') as f:
+    n = len(y1) # length of the signal
+    k = np.arange(n)
+    T = n/Fs
+    frq = k/T # two sides frequency range
+    frq = frq[range(int(n/2))] # one side frequency range
+    Y = np.fft.fft(y1)/n # fft computing and normalization
+    Y1 = Y[range(int(n/2))]
+
+    n = len(y2) # length of the signal
+    k = np.arange(n)
+    T = n/Fs
+    frq = k/T # two sides frequency range
+    frq = frq[range(int(n/2))] # one side frequency range
+    Y = np.fft.fft(y2)/n # fft computing and normalization
+    Y2 = Y[range(int(n/2))]
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
+    ax1.plot(t1,y1,'b')
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Amplitude')
+    ax1.set_title('Raw Data vs Filtered Data (MAF)')
+    ax2.loglog(frq,abs(Y1),'b') # plotting the fft
+    ax2.set_xlabel('Freq (Hz)')
+    ax2.set_ylabel('|Y(freq)|')
+
+    ax3.plot(t2,y2,'r')
+    ax3.set_xlabel('Time')
+    ax3.set_ylabel('Amplitude')
+    ax4.loglog(frq,abs(Y2),'r') # plotting the fft
+    ax4.set_xlabel('Freq (Hz)')
+    ax4.set_ylabel('|Y(freq)|')
+    
+    plt.show()
+
+def MAF(data1, x):
+    x = 1000
+    filtered = []
+    for index in range(len(data1)):
+        sum = 0
+        if index < x: # check if there are enough data points
+            for count in range(index):
+                sum = sum + data1[index - count]
+            average = sum/x
+        else:
+            for count in range(x):
+                sum = sum + data1[index - count]
+            average = sum/x
+        filtered.append(average)
+    return filtered
+
+def get_data(file):
+    t = [] # column 0
+    data1 = [] # column 1
+    with open(file) as f:
     # open the csv file
-    reader = csv.reader(f)
-    for row in reader:
-        # read the rows 1 one by one
-        t.append(float(row[0])) # leftmost column
-        data1.append(float(row[1])) # second column
+        reader = csv.reader(f)
+        for row in reader:
+            # read the rows 1 one by one
+            t.append(float(row[0])) # leftmost column
+            data1.append(float(row[1])) # second column
+    return t, data1
 
-x = 1000
-filtered = []
-for index in range(len(data1)):
-    sum = 0
-    if index < x: # check if there are enough data points
-        for count in range(index):
-            sum = sum + data1[index - count]
-        average = sum/x
-    else:
-        for count in range(x):
-            sum = sum + data1[index - count]
-        average = sum/x
-    filtered.append(average)
-    # print(average)
+t, data1 = get_data('sigA.csv')
+
+filtered_MAF = MAF(data1, 5000)
 
 
-plt.figure(1)
-plt.plot(t,data1,'b-*')
-plt.xlabel('Time [s]')
-plt.ylabel('Signal')
-plt.title('Signal vs Time')
+fft2(t, data1, t, filtered_MAF)
 
 
-plt.figure(2)
-plt.plot(t,filtered,'r-*')
-plt.xlabel('Time [s]')
-plt.ylabel('Signal')
-plt.title('Signal vs Time')
-
-plt.show()
+    
 
 sampling_rate = len(t)/t[-1]
 print("Sampling rate: " + str(int(sampling_rate)) + " samples per second")
+
+
--- HW9/python_fft.py
+++ HW9/python_fft.py
@@ -1,13 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
 
-dt = 1.0/100.0 # 10kHz
+dt = 1.0/10000.0 # 10kHz
 t = np.arange(0.0, 1.0, dt) # 10s
 # a constant plus 100Hz and 1000Hz
 s = 4.0 * np.sin(2 * np.pi * 100 * t) + 0.25 * np.sin(2 * np.pi * 1000 * t) + 25 # data source
 
-Fs = 100 # sample rate
+Fs = 10000 # sample rate
 Ts = 1.0/Fs; # sampling interval
 ts = np.arange(0,t[-1],Ts) # time vector (already in csv files)
 
@@ -29,4 +28,3 @@ ax2.loglog(frq,abs(Y),'b') # plotting the fft
 ax2.set_xlabel('Freq (Hz)')
 ax2.set_ylabel('|Y(freq)|')
 plt.show()
-
